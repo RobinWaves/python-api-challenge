bb1d9af9bcf475f10d946f591de5530f78eb240a
Robin Christoffersen <robin.christoffersen@gmail.com>
2021-04-17 21:31:32 -0700

    Updated City DataFrame loaded and data pulled from Weather API

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "7f6b2c6fc02a94544d7cd68ca2bb433d"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCM8Np0QvUfvx-oWYdb8wJ8-qXm4ovYVd0"
